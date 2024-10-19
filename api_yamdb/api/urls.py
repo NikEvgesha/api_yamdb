d9d021cc25da76231caf9d79a1f28dee9db96f81
NikEvgesha <nikitenkowa.e@yandex.ru>
2024-10-19 18:22:37 +0300

    users/api все тесты прошли

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -4,7 +4,6 @@ from rest_framework.routers import DefaultRouter
 from users.views import UserViewSet, UserSignup, GetToken
 
 router_v1 = DefaultRouter()
-#  router_v1.register('users/me', UserMeViewSet, basename='me')
 router_v1.register('users', UserViewSet, basename='user')
 
 urlpatterns_v1 = [
--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -1,10 +1,13 @@
+import hashlib
+from datetime import datetime
+
 from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
 from django.db.models import Q
-from rest_framework import viewsets, status, mixins, generics
+from rest_framework import viewsets, status, mixins, generics, filters
+from rest_framework.decorators import action
 from rest_framework.response import Response
-# from rest_framework.decorators import action
-# from django.views.decorators.csrf import csrf_exempt
+from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
 
@@ -12,16 +15,37 @@ from users.models import User
 from users.serializers import (
     UserSerializer,
     UserSignupSerializer,
-    UserTokenSerializer
+    UserTokenSerializer,
+    UserMeSerializer
 )
-from users.permissions import IsAdmin
+from users.permissions import IsAdmin, IsSuperuser
+from users.pagination import UserPagination
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
+    queryset = User.objects.all().order_by('id')
     serializer_class = UserSerializer
     lookup_field = 'username'
-    permission_classes = [IsAuthenticated, IsAdmin]
+    permission_classes = [IsAuthenticated, IsAdmin | IsSuperuser]
+    http_method_names = ['get', 'post', 'patch', 'delete']
+    pagination_class = UserPagination
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('username',)
+
+    @action(url_path='me',
+            detail=False,
+            permission_classes=[IsAuthenticated,],
+            methods=['GET', 'PATCH'])
+    def me(self, request, *args, **kwargs):
+        user = get_object_or_404(User, pk=request.user.id)
+        if request.method == "GET":
+            serializer = UserMeSerializer(user)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        serializer = UserMeSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response('Неверные данные', status=status.HTTP_400_BAD_REQUEST)
 
 
 class RetrieveUpdateViewSet(
@@ -36,6 +60,13 @@ class CreateViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     pass
 
 
+class RetrieveUpdateViewSet(
+        mixins.RetrieveModelMixin,
+        mixins.UpdateModelMixin,
+        viewsets.GenericViewSet):
+    pass
+
+
 class UserSignup(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSignupSerializer
@@ -43,11 +74,11 @@ class UserSignup(generics.CreateAPIView):
     def post(self, request):
         username = request.data.get('username')
         email = request.data.get('email')
-        confirmation_code = '1234'
 
         user = User.objects.filter(username=username, email=email)
         if user.exists():
             user = user[0]
+            confirmation_code = generate_confirmation_code(username)
             user.confirmation_code = confirmation_code
             user.save()
             send_confirmation_code(user)
@@ -56,6 +87,8 @@ class UserSignup(generics.CreateAPIView):
                 status=status.HTTP_200_OK
             )
         else:
+            serializer = UserSignupSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
             user_duplicates = User.objects.filter(
                 Q(username=username)
                 | Q(email=email)
@@ -65,10 +98,9 @@ class UserSignup(generics.CreateAPIView):
                     'Неверные данные',
                     status=status.HTTP_400_BAD_REQUEST
                 )
-            serializer = UserSignupSerializer(data=request.data)
-            serializer.is_valid(raise_exception=True)
             serializer.save()
             user = get_object_or_404(User, username=username)
+            confirmation_code = generate_confirmation_code(username)
             user.confirmation_code = confirmation_code
             user.save()
             send_confirmation_code(user)
@@ -84,10 +116,26 @@ class GetToken(generics.CreateAPIView):
 
     def post(self, request):
         username = request.data.get('username')
-        user = get_object_or_404(User, username=username)
-        serializer = UserTokenSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        return Response(get_token(user), status=status.HTTP_201_CREATED)
+        confirmation_code = request.data.get('confirmation_code')
+        if not username or not confirmation_code:
+            return Response(
+                'Поля username и confirmation_code являются обязательными!',
+                status=status.HTTP_400_BAD_REQUEST)
+        user = User.objects.filter(username=username)
+        if user.exists():
+            serializer = UserTokenSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            return Response(get_token(user), status=status.HTTP_201_CREATED)
+        return Response('Некорректные данные',
+                        status=status.HTTP_404_NOT_FOUND)
+
+
+def generate_confirmation_code(username):
+    timestamp = datetime.now().timestamp()
+    fields = (username, str(timestamp))
+    data = ''.join(fields).encode('utf-8')
+    code = hashlib.sha256(data).hexdigest()
+    return code
 
 
 def send_confirmation_code(user):
@@ -98,10 +146,6 @@ def send_confirmation_code(user):
         [user.email,],
         fail_silently=False
     )
-
-
-def token():
-    ...
 
 
 def get_token(user):
