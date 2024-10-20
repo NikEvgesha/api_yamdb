10c4080a2b30148861f96f5f155a76e90a099f14
Nikitenkova Evgeniya <91124072+NikEvgesha@users.noreply.github.com>
2024-10-20 22:46:15 +0300

    Update import_csv.py

--- api_yamdb/import_csv.py
+++ api_yamdb/import_csv.py
@@ -73,11 +73,12 @@ with open('static/data/review.csv', 'r', encoding='utf-8') as f:
     next(reader) 
     for row in reader:
         title_obj = Title.objects.get(id=row[1])
+        author_obj = User.objects.get(id=row[3])
         obj = Review.objects.get_or_create(
             id=row[0],
             title_id=title_obj.id,
             text=row[2],
-            author=row[3],
+            author=author_obj,
             score=row[4],
             pub_date=row[5]
         )
@@ -87,10 +88,11 @@ with open('static/data/comments.csv', 'r', encoding='utf-8') as f:
     next(reader) 
     for row in reader:
         review_obj = Review.objects.get(id=row[1])
+        author_obj = User.objects.get(id=row[3])
         obj = Comment.objects.get_or_create(
             id=row[0],
             review_id=review_obj.id,
             text=row[2],
-            author=row[3],
+            author=author_obj,
             pub_date=row[4]
         )
